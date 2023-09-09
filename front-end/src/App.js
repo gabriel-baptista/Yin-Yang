513ebedfc007ebc710c3ecb3634829509fd10745
Gabriel Baptista <gbdso16@gmail.com>
2023-09-09 17:17:23 -0300

    style(pacientesView): vizualização da tabela de clientes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,7 +7,7 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidemenu from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/Team";
+import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
@@ -32,7 +32,7 @@ const App = () => {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} /> */}
+              <Route path="/team" element={<Team />} />
               {/* <Route path="/contacts" element={<Contacts />} /> */}
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
